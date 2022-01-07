b96e6b4cd92f39f2a4941a9be4a8927f73a89af0
marcoleitecarmo <marcoleitecarmo@gmail.com>
2022-01-07 16:23:39 -0300

    jogo de futebolpong finalizado

--- Jogos/main.py
+++ Jogos/main.py
@@ -3,8 +3,14 @@ import pygame
 pygame.init()
 
 window = pygame.display.set_mode([1280, 720])
-
 title = pygame.display.set_caption("Futebol Pong")
+
+win = pygame.image.load('assentz/win.png')
+
+score1 = 0
+score1_img = pygame.image.load('assentz/score/0.png')
+score2 = 0
+score2_img = pygame.image.load('assentz/score/0.png')
 
 field = pygame.image.load('assentz/field.png')
 
@@ -19,7 +25,7 @@ player2_y = 310
 ball = pygame.image.load('assentz/ball.png')
 ball_x = 617
 ball_y = 337
-ball_dir = -3
+ball_dir = -10
 ball_dir_y = 1
 
 
@@ -42,37 +48,69 @@ def move_player():
         player1_y = 575
 
 
+def move_player2():
+    global player2_y
+    player2_y = ball_y
+
+
 def move_ball():
     global ball_x
     global ball_y
     global ball_dir
     global ball_dir_y
+    global score1
+    global score2
+    global score2_img
+    global score1_img
 
     ball_x += ball_dir
     ball_y += ball_dir_y
 
-
     if ball_x < 120:
-        if player2_y < ball_y + 23:
-            if player2_y + 146> ball_y:
-                ball_dir *= - 1
-
-    if ball_x > 1100:
         if player1_y < ball_y + 23:
             if player1_y + 146 > ball_y:
                 ball_dir *= - 1
 
-    if ball_y > 685:
+    if ball_x > 1100:
+        if player2_y < ball_y + 23:
+            if player2_y + 146 > ball_y:
+                ball_dir *= - 1
+
+    if ball_y > 665:
         ball_dir_y *= -1
     elif ball_y <= 0:
         ball_dir_y *= -1
 
+    if ball_x < -50:
+        ball_x = 617
+        ball_y = 337
+        ball_dir_y *= -1
+        ball_dir *= -1
+        score2 += 1
+        score2_img = pygame.image.load('assentz/score/' + str(score2) + '.png')
+
+    elif ball_x > 1320:
+        ball_x = 617
+        ball_y = 337
+        ball_dir_y *= -1
+        ball_dir *= -1
+        score1 += 1
+        score1_img = pygame.image.load('assentz/score/' + str(score1) + '.png')
+
 
 def draw():
-    window.blit(field, (0, 0))
-    window.blit(player1, (50, player1_y))
-    window.blit(player2, (1150, player2_y))
-    window.blit(ball, (ball_x, ball_y))
+    if score1 or score2 < 9:
+        window.blit(field, (0, 0))
+        window.blit(player1, (50, player1_y))
+        window.blit(player2, (1150, player2_y))
+        window.blit(ball, (ball_x, ball_y))
+        window.blit(score1_img, (500, 50))
+        window.blit(score2_img, (710, 50))
+        move_ball()
+        move_player()
+        move_player2()
+    else:
+        window.blit(win, (300, 330))
 
 
 loop = True
@@ -93,6 +131,4 @@ while loop:
                 player1_movedown = False
 
     draw()
-    move_ball()
-    move_player()
     pygame.display.update()
